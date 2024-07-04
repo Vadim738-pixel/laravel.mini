74cfb592a0deb2234615d7eca7b1bca858285acc
Vadim738-pixel/extra <vadim.kostinyuk@gmail.com>
2024-07-04 14:29:08 +0300

    maked changes back to view

--- database/migrations/2024_07_01_022824_create_posts_table.php
+++ database/migrations/2024_07_01_022824_create_posts_table.php
@@ -24,11 +24,7 @@ class CreatePostsTable extends Migration
 
             $table->softDeletes();
 
-            $table->unsignedBigInteger('category_id')->nullable();
 
-            $table->index('category_id', 'post_category_idx');
-
-            $table->foreign('category_id', 'post_category_fk')->on('categories')->references('id');
         });
     }
 
